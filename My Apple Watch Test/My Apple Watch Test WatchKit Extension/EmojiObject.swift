d808023e63d342f1757b81fe27a500789672b052
Lorenz Hänggi <lolo8304@gmail.com>
2015-03-10 01:04:59 +0100

    emoji objects

---
+++ My Apple Watch Test/My Apple Watch Test WatchKit Extension/EmojiObject.swift
@@ -0,0 +1,26 @@
+//
+//  EmojiObject.swift
+//  My Apple Watch Test
+//
+//  Created by Lorenz Hänggi on 09/03/15.
+//  Copyright (c) 2015 Lorenz Hänggi. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class EmojiObject: NSObject {
+    
+    class func emojis() -> Array<Array<String>> {
+        var emojis = [ ["😄" , "smiley"], ["🐹" , "Hamster"], ["🏣" , "house"]]
+        return emojis;
+    }
+    class func rand_emoji() -> Array<String> {
+        sranddev()
+        let rIndex = Int(rand())  % EmojiObject.emojis().count
+        return EmojiObject.emojis()[rIndex]
+    }
+    
+    @IBOutlet weak var emojiLabel: WKInterfaceLabel!
+    @IBOutlet weak var emojiDescription: WKInterfaceLabel!
+}

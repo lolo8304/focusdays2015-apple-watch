260b91369711e3294006a7363ac7e61e1171e910
Lorenz Hänggi <lolo8304@gmail.com>
2015-03-10 01:05:35 +0100

    zoom emoji

---
+++ My Apple Watch Test/My Apple Watch Test WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,44 @@
+//
+//  InterfaceController.swift
+//  My Apple Watch Test WatchKit Extension
+//
+//  Created by Lorenz Hänggi on 09/03/15.
+//  Copyright (c) 2015 Lorenz Hänggi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var table: WKInterfaceTable!
+    
+    var emojis = EmojiObject.emojis();
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        self.table.setNumberOfRows(self.emojis.count, withRowType: "EmojiObject")
+        for index in 0..<self.emojis.count {
+            var emoji = self.table.rowControllerAtIndex(index) as EmojiObject
+            emoji.emojiLabel.setText(emojis[index][0])
+            emoji.emojiDescription.setText(emojis[index][1])
+        }
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+        
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        self.pushControllerWithName("ZoomEmojiController", context: self.emojis[rowIndex])
+    }
+
+}
---
+++ My Apple Watch Test/My Apple Watch Test WatchKit Extension/ZoomEmojiController.swift
@@ -0,0 +1,40 @@
+//
+//  ZoomEmojiController.swift
+//  My Apple Watch Test
+//
+//  Created by Lorenz Hänggi on 09/03/15.
+//  Copyright (c) 2015 Lorenz Hänggi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ZoomEmojiController: WKInterfaceController {
+
+    @IBOutlet weak var zoomEmojiLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var zoomEmojiDescription: WKInterfaceLabel!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        var s = context as Array<String>
+        
+        var font = UIFont.systemFontOfSize(100)
+        var zoomString = NSAttributedString(string: s[0], attributes: [NSFontAttributeName: font])
+
+        self.zoomEmojiLabel.setAttributedText(zoomString)
+        self.zoomEmojiDescription.setText(s[1])
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
